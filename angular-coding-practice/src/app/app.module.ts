07c8bc17a310912ea52735375ebf601cb8644a4d
devdayal367 <dayaldev376@gmail.com>
2022-04-09 23:16:21 +0530

    #1 components seperation, basic styling and images loading completed

--- angular-coding-practice/src/app/app.module.ts
+++ angular-coding-practice/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { GalleryViewComponent } from './components/main-view/gallery-view/gallery-view.component';
+import { PhotoDetailViewComponent } from './components/main-view/photo-detail-view/photo-detail-view.component';
+import { MainViewComponent } from './components/main-view/main-view/main-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    NavbarComponent
+    NavbarComponent,
+    GalleryViewComponent,
+    PhotoDetailViewComponent,
+    MainViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-coding-practice/src/app/components/main-view/photo-detail-view/photo-detail-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-detail-view',
+  templateUrl: './photo-detail-view.component.html',
+  styleUrls: ['./photo-detail-view.component.scss']
+})
+export class PhotoDetailViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
