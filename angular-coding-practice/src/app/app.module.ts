9df25e0b7741980754d8b19486399d84e06d6f1e
devdayal367 <dayaldev376@gmail.com>
2022-04-09 12:56:06 +0530

    #1 created componenrts for sidebar and navbar and seperated html files

--- angular-coding-practice/src/app/app.module.ts
+++ angular-coding-practice/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
